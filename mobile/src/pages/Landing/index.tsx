c8076d59c7c119ccbd77da9eaafba54e061145c4
Felipe Alves Barboza <50283187+felipeab03@users.noreply.github.com>
2020-08-07 11:47:25 -0300

    Final commit

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
@@ -8,9 +8,20 @@ import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 const Landing: React.FC = () =>{
   const navigation = useNavigation();
+
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(()=>{
+    api.get('connections').then(response=>{
+      const {total} = response.data;
+      
+      setTotalConnections(total);
+    });
+  }, []);
 
   function handleNavigateToGiveClassesPage(){
     navigation.navigate('GiveClasses');
@@ -44,7 +55,7 @@ const Landing: React.FC = () =>{
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 280 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,13 +1,123 @@
-import React from 'react';
-import {View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import styles from './styles';
 import PageHeader from '../../components/PageHeader';
+import { ScrollView, TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import api from '../../services/api';
+import { useFocusEffect } from '@react-navigation/native';
 
 const TeacherList: React.FC = () => {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+  const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function loadFavorites(){
+    AsyncStorage.getItem('favorites').then(response=>{
+      if(response){
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher)=>{
+          return teacher.id;
+        })
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  useFocusEffect(()=>{
+    loadFavorites();
+  })
+
+  async function handleFiltersSubmit(){
+    loadFavorites();
+    const response = await api.get('classes', {
+      params:{
+        subject, week_day, time,
+      }
+    });
+    setIsFiltersVisible(false);
+    setTeachers(response.data);
+  }
+
+  function handleToggleFiltersVisible(){
+    setIsFiltersVisible(!isFiltersVisible);
+  }
+
   return (
       <View style={styles.container}>
-        <PageHeader title="Proffys disponíveis"/>
+        <PageHeader 
+          title="Proffys disponíveis" 
+          headerRight={(
+            <BorderlessButton onPress={handleToggleFiltersVisible}>
+              <Feather size={20} name="filter" color="#FFF" />
+            </BorderlessButton>
+          )}
+        >
+          {isFiltersVisible && ( 
+            <View style={styles.searchForm}>
+              <Text style={styles.label}>Matéria</Text>
+              <TextInput 
+                value={subject} 
+                onChangeText={text=>setSubject(text)} 
+                style={styles.input} 
+                placeholderTextColor="#c1bccc" 
+                placeholder="Qual a matéria?"
+              />
+
+              <View style={styles.inputGroup}>
+                <View style={styles.inputBlock}>
+                  <Text style={styles.label}>Dia da semana</Text>
+                  <TextInput 
+                    value={week_day} 
+                    onChangeText={text=>setWeekDay(text)} 
+                    style={styles.input} 
+                    placeholderTextColor="#c1bccc" 
+                    placeholder="Qual o dia?"
+                  />
+                </View>
+
+                <View style={styles.inputBlock}>
+                  <Text style={styles.label}>Horário</Text>
+                  <TextInput 
+                    value={time} 
+                    onChangeText={text=>setTime(text)} 
+                    style={styles.input} 
+                    placeholderTextColor="#c1bccc" 
+                    placeholder="Qual horário?"
+                  />
+                </View>
+              </View>
+
+              <RectButton onPress={handleFiltersSubmit} style={styles.submitButton}>
+                <Text style={styles.submitButtonText}>Filtrar</Text>
+              </RectButton>
+            </View>
+          )}
+        </PageHeader>
+
+        <ScrollView 
+          style={styles.teacherList} 
+          contentContainerStyle={{
+            paddingHorizontal: 16,
+            paddingBottom: 16
+          }} 
+        >
+          {teachers.map((teacher: Teacher)=> {
+            return(
+              <TeacherItem 
+                key={teacher.id} 
+                teacher={teacher}
+                favorited={favorites.includes(teacher.id)}
+              />
+            )
+          })}
+        </ScrollView>
       </View>
   );
 };
